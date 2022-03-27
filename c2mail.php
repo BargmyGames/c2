a750c4ec256975512ae9dc03f2f9a294c3bb4ee6
BargmyDL <94040843+BargmyGames@users.noreply.github.com>
2022-03-27 02:24:30 -1200

    c2 php

---
+++ c2mail.php
@@ -0,0 +1,15 @@
+
+<?php
+$to       = $_POST['email_to']
+$subject  =   $_POST['subject']
+$massage  =  $_POST['message']
+$headers  =  'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+
+echo "Message sent at ";
+echo date ("h:i:s");
+
+?>
